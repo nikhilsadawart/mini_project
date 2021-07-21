eb024c04e8ce92ee5748acc45295b69f5f00c707
nikhilsadawart <83805172+nikhilsadawart@users.noreply.github.com>
2021-07-21 14:55:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ standard_to = StandardScaler()
 def predict():
     Fuel_Type_Diesel=0
     if request.method == 'POST':
-        Car_Name=np.character(request.form['Car_Name'])
         Year = int(request.form['Year'])
         Present_Price=float(request.form['Present_Price'])
         Kms_Driven=int(request.form['Kms_Driven'])
@@ -41,7 +40,7 @@ def predict():
             Transmission_Mannual=1
         else:
             Transmission_Mannual=0
-        prediction=model.predict([[Car_Name,Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
